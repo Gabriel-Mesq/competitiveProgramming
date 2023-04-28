dacc16ab5b0e1ca794b29ac80f4f15dd1ebd023e
Gabriel Mesquita <90516048+Gabriel-Mesq@users.noreply.github.com>
2023-04-28 16:11:44 -0300

    Update main.py

--- tap-01/4-Is your horseshoe on the other hoof?/main.py
+++ tap-01/4-Is your horseshoe on the other hoof?/main.py
@@ -1,1 +1,10 @@
+cores = [int(i) for i in input().split()]
+rep = set()
+ct = 0 
 
+for cor in cores:
+    if cores.count(cor) > 1 and cor not in rep:
+        ct += cores.count(cor) - 1
+        rep.add(cor)
+
+print(ct)     
