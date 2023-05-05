251c75db75601b84dba7137855291127c4356e89
Gabriel Mesq <gabriel.mesquita@live.com>
2023-05-05 14:47:38 -0300

    abada

--- tap-01/9-TEX Quotes/main.py
+++
@@ -1,9 +0,0 @@
-#Problema para strings de multiplas linhas 
-
-texto = str(input())
-
-for _ in range (texto.count("")):
-    texto = texto.replace("\"", "``", 1)
-    texto = texto.replace("\"", "''", 1)
-
-print(texto)
